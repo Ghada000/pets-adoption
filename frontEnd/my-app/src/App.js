743fbb8c60274fe0d38d00c91aa80d29d4ab137d
Ghada000 <assidighada4@gmail.com>
2024-02-02 21:48:25 +0100

    updatesss

--- frontEnd/my-app/src/App.js
+++ frontEnd/my-app/src/App.js
@@ -1,21 +1,33 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './Components/Nav.jsx';
 import Slider from './Components/Slider.jsx';
-import Products from './Components/Products.jsx';
-// import Login from '../src/Login.jsx';
+
+import Chat from './Components/Chat.jsx';
+import AboutUs from './Components/AboutUs.jsx';
+import ProductsFishes from './Components/ProductsFishes.jsx';
+import ProductsDogs from './Components/ProductsDogs.jsx';
+import ProductsCats from './Components/ProductsCats.jsx';
 
 
 function App() {
+  const [view, setView] = useState("");
+
+  const changeView = (newView) => {
+    setView(newView);
+  };
+
   return (
     <div className="App">
-      <Nav/>
+      <Nav changeView={changeView} />
+      <Slider />
+      {view === "Chat" ? <Chat /> : null}
+      {view === "ProductsDogs" ? <ProductsDogs /> : null}
+      {view === "ProductsCats" ? <ProductsCats /> : null}
+      {view === "ProductsFishes" ? <ProductsFishes/> : null}
 
-      <Slider/>
-      <Products/>
+    
 
-      
-      
     </div>
   );
 }
---
+++ frontEnd/my-app/src/Components/ProductsFishes.jsx
@@ -0,0 +1,59 @@
+import React ,{useEffect,useState}from 'react'
+import { FaRegHeart } from "react-icons/fa";
+import axios from 'axios'
+
+export default function ProductsFishes() {
+const [data,setData]=useState([])
+useEffect(()=>{
+axios
+.get('http://localhost:5000/api/fishes')
+.then ((res)=>{
+  setData(res.data)
+})
+.catch((error)=>{
+  console.log(error);
+})
+},[])
+console.log(data);
+
+
+
+
+
+  return (
+    <div >
+
+
+    <h1> Our Fishes </h1>
+
+    <div className='List-Product'>
+{data.map((e,i)=>{
+return (
+  <div className='product'>
+
+
+  <img src={e.image_url}  />
+    <div className='heart'>
+    <h4 className='nameof'>{e.name}</h4>
+    <FaRegHeart className='h'/>
+    </div>
+    <div className='class'>
+<button>Update</button>
+<button>Delete</button>
+
+
+    </div>
+    
+    </div> )
+    })}
+    
+    
+   
+    
+    
+    </div>
+
+    
+    </div>
+  )
+}
