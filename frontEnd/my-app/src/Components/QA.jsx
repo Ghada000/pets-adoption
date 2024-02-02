f49c92d72ae50d2c047ed05fde4a7a85dd90e929
JALLELI-HIBA <jalleli.hibaa@gmail.com>
2024-02-02 16:14:17 +0100

    new

---
+++ frontEnd/my-app/src/Components/QA.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function QA() {
+  return (
+    <div>
+      <h1>Questions and Answers Page</h1>
+      {/* Add your QA component content here */}
+    </div>
+  );
+}
