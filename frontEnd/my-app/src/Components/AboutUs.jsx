b6d6eeeebd850680da8293f1cef4ea100f41ed23
JALLELI-HIBA <jalleli.hibaa@gmail.com>
2024-02-04 19:26:17 +0100

    changes and addes

--- frontEnd/my-app/src/Components/AboutUs.jsx
+++ frontEnd/my-app/src/Components/AboutUs.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import qrCodeImage from '../QR/QR.png';
-import aboutUsImage from '../QR/rabit.jpg'
-import './AboutUs.css'
-const AboutUs = () => {
-  
+import aboutUsImage from '../QR/rabit.jpg';
+import './AboutUs.css';
 
-  
+const AboutUs = () => {
   return (
     <div className="about-us-container">
       <div className="text-container">
@@ -23,8 +21,8 @@ const AboutUs = () => {
         <div className="about-us-text">
           <h2>About Us</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla fermentum felis in massa volutpat, a aliquet odio commodo. 
-            Sed tincidunt elit nec risus feugiat, eu fermentum justo cursus.
+          Save-A-Life, Inc. is an all volunteer IRS 501 (c)(3) non-profit animal welfare organization created to prevent overpopulation of companion animals through low-cost spaying and neutering. For more than 5 decades Save-A-Life has assumed a leadership role in advocating low-cost spaying and neutering in order to help prevent unwanted, homeless dogs and cats. Responsible pet owners do not add to the pet overpopulation problem, they spay or neuter their pets. Save-A-Life also provides foster care, necessary medical treatment for foster animals, and a placement assistance program to help members of the public place their companion animals. All funding for SAL programs comes from donations and fund raising activities. Save-A-Life, Inc. is licensed by the Georgia Department of Agriculture. We have been saving animal lives since 1967!
+            Save A Life Animal Welfare
           </p>
         </div>
         <div className="about-us-image">
@@ -36,4 +34,3 @@ const AboutUs = () => {
 };
 
 export default AboutUs;
-
